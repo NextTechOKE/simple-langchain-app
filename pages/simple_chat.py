d6d8aaa20109eb0c9d476f06f5739557bbfb0450
NextTechOKE <Jacewilliams7144@gmail.com>
2023-10-09 18:38:07 -0400

    Prompt

--- pages/simple_chat.py
+++ pages/simple_chat.py
@@ -18,7 +18,7 @@ llm_prompt = ChatPromptTemplate(
     messages=[
         SystemMessagePromptTemplate.from_template(
             # This prompt tells the chatbot how to respond. Try modifying it.
-            "You are an AI assistant."
+            "You are an AI therapy bot and you want to be as helpfull and comforting as possible."
         ),
         HumanMessagePromptTemplate.from_template("{message}")
     ]
